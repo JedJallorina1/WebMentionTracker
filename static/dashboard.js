8e0006ff169b202d3e714ba8e04219c2dbaf511e
Jed Jallorina <jedrikjallorina@gmail.com>
2026-08-14 21:15:04 -0400

    aesthetic changes

--- static/dashboard.js
+++ static/dashboard.js
@@ -1,6 +1,18 @@
+// POC: Jed Jallorina
 const configureSearch = document.getElementById("configure-search");
+const viewAllMentions = document.getElementById("view-all-mentions");
+const getHelp = document.getElementById("help");
 
 configureSearch.addEventListener("click", function()
 {
     window.location.href = "/setup"
 });
+viewAllMentions.addEventListener("click", function()
+{
+    // navigate to all mentions page
+});
+getHelp.addEventListener("click", function()
+{
+    // show popup
+});
+
